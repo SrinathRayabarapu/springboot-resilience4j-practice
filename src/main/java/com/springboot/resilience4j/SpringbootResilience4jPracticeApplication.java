de5f2937231fea86f9cbff9eccc56f2e56fa3d0d
Srinath Rayabarapu <srinath.rayabarapu@tesco.com>
2022-03-29 00:51:27 +0530

    Initial commit

---
+++ src/main/java/com/springboot/resilience4j/SpringbootResilience4jPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootResilience4jPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootResilience4jPracticeApplication.class, args);
+	}
+
+}
