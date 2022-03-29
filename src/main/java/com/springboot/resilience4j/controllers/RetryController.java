31fb7a459001bec62af9995b1c349a34985aa211
Srinath Rayabarapu <srinath.rayabarapu@tesco.com>
2022-03-30 00:52:04 +0530

    RetryController and it's configuration added to application.yml

--- src/main/java/com/springboot/resilience4j/controllers/RetryController.java
+++ src/main/java/com/springboot/resilience4j/controllers/RetryController.java
@@ -1,7 +1,31 @@
 package com.springboot.resilience4j.controllers;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Log4j2
+@RestController
 public class RetryController {
 
+    RestTemplate restTemplate = new RestTemplate();
 
+    @GetMapping("/getInvoice")
+    @Retry(name = "getInvoiceRetry", fallbackMethod = "getInvoiceFallback")
+    public String getInvoice() {
+        log.info("getInvoice() call starts here");
+        // this url available in "spring-boot-security" repo
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://localhost:8081/user", String.class);
+        log.info("Response :" + entity.getStatusCode());
+        return entity.getBody();
+    }
+
+    public String getInvoiceFallback(Exception e) {
+        log.info("---RESPONSE FROM FALLBACK METHOD--- {}", e.getMessage());
+        return "SERVICE IS DOWN, PLEASE TRY AFTER SOMETIME !!!";
+    }
 
 }
